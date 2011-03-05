9ac0ef4759a3ae0d4ec0107b80d41cea3deb171c
Richard <willis.rh@gmail.com>
2011-03-05 21:16:25 +0000

    updated views

--- application/views/footer.php
+++ application/views/footer.php
@@ -1,15 +1,12 @@
 <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.5.1/jquery.min.js"></script>
 <script type="text/javascript">
 	(function(window, $){
-	
 		var optionContainers = $('.options-container');
-
-		$('#choose-compressor')
+		$('#compressor')
 			.bind('change.compressor', function(){
 				optionContainers.hide();
 				$('#options-' + this.value).show();
 			})
 			.trigger('change.compressor');	
-
 	})(this, this.jQuery);
 </script>
--- application/views/home.php
+++ application/views/home.php
@@ -19,29 +19,49 @@
 				<li>
 					<h2><label for="choose-compressor">Choose your compressor:</label></h2>
 					<div class="field">
-						<select name="compressor" id="choose-compressor">
-							<option value="closure">Closure compiler</option>
-							<option value="yui">YUI compressor</option>
-							<option value="packer">Packer</option>
-							<option value="uglify">Uglify</option>
-						</select>
+						<?php echo Form::select('compressor', $compressors, @$_POST['compressor'])?>
 					</div>
 				</li>
 				<li>
-					<h2>Set the compressor options:</h2>
+					<h2>Set the compressor options:</h2><br />
 					<fieldset class="options-container" id="options-yui">
 						<p>Type:</p>
 						<div class="field"><label><input type="radio" name="option-yui-type" value="js" checked="checked" /> Javascript</label></div>
 						<div class="field"><label><input type="radio" name="option-yui-type" value="css" /> CSS</label></div>
 					</fieldset>
 					<fieldset class="options-container" id="options-closure">
-						closure
+						<div class="field">
+							<label>
+								Compilation level
+								<?php echo Form::select('options_closure_compilation_level', $options_closure_compilation_levels, @$_POST['options_closure_compilation_level']);?>
+							</label>
+						</div>
+						<div class="field">
+							<label>
+								Warning level
+								<?php echo Form::select('options_closure_warning_level', $options_closure_warning_levels, @$_POST['options_closure_warning_level']);?>
+							</label>
+						</div>
+						<div class="field">
+							<label>
+								<?php echo Form::input('options-closure-pretty_print', NULL, array('type' => 'checkbox'), $errors)?>
+								Pretty print
+							</label>
+						</div>
+					</fieldset>
+					<fieldset class="options-container" id="options-uglify">
+						<div class="field"><label><input type="checkbox" name="option-uglify-beautify" /> Beautify</label></div>
+						<div class="field"><label><input type="checkbox" name="option-uglify-nomangle" /> No mangle</label></div>
+						<div class="field"><label><input type="checkbox" name="option-uglify-nocopyright" /> No copyright</label></div>
+					</fieldset>
+					<fieldset class="options-container" id="options-all">
+						<p><em>(Using default options)</em></p>
 					</fieldset>
 				</li>
 				<li>
 					<h2><label for="code">Paste your code:</label></h2>
 					<div class="field">
-						<textarea name="code" id="code"></textarea>
+						<?php echo Form::textarea('code', @$_POST['code'], NULL, TRUE, $errors)?>
 					</div>
 				</li>
 				<li>
