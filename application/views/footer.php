8868d5658ca224334e9a1ca212592bf23c3848ac
Richard <willis.rh@gmail.com>
2011-05-19 20:43:12 +0000

    removed jquery from script file, linked to cdn version

--- application/views/footer.php
+++ application/views/footer.php
@@ -1,6 +1,7 @@
 <footer>
 <?php echo View::factory('navigation')?>
 </footer>
+<script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/jquery/1.6.1/jquery.min.js"></script>
 <script type="text/javascript" src="/js/script.js"></script>
 <script type="text/javascript">
 (function(){
