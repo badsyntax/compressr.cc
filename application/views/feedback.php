f3c7bee8a3b05c6ff2c2dfc82c9996f1a7a0ab6c
Richard <willis.rh@gmail.com>
2011-05-19 20:58:44 +0000

    updated feedback page

--- application/views/feedback.php
+++ application/views/feedback.php
@@ -1,6 +1,7 @@
 <div class="container box clear">
 	<div class="bg">
 		<p>Send emails to: <a href="mailto:willis.rh@gmail.com">willis.rh@gmail.com</a></p>
-		<p class="nomarg">Follow me on twitter: <a href="http://twitter.com/badsyntax">@badsyntax</a></p>
+		<p>Use the <a href="https://github.com/badsyntax/compressr.cc/issues">issue tracker</a> to let me know if stuff isn't working as expected.</p>
+		<p>View the <a href="https://github.com/badsyntax/compressr.cc">source code</a> on github.</a>
 	</div>
 </div>
